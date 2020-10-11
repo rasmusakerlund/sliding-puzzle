47f40039f756a05c1bddd2ca43346fba2a1907dd
Rasmus Åkerlund <rasmus.akerlund@gmail.com>
2020-10-11 09:21:14 +0200

    Updates

---
+++ precache-manifest.e627b27180bcef87f56865276e1a5b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba55a20be02adec80fb8c82cd953657c",
+    "url": "/sliding-puzzle/index.html"
+  },
+  {
+    "revision": "66219e8e6126f3888f5c",
+    "url": "/sliding-puzzle/static/css/main.627de45f.chunk.css"
+  },
+  {
+    "revision": "ed7fb7e6f3973d86c117",
+    "url": "/sliding-puzzle/static/js/2.e252d2ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sliding-puzzle/static/js/2.e252d2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66219e8e6126f3888f5c",
+    "url": "/sliding-puzzle/static/js/main.3547928d.chunk.js"
+  },
+  {
+    "revision": "39c5cc059bf749f5cb82",
+    "url": "/sliding-puzzle/static/js/runtime-main.284da306.js"
+  }
+]);
